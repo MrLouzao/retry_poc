c61ca6621a4ad6eee47314e902632372cef283df
MrLouzao <luismiguel.louzao.gonzalez@gmail.com>
2020-02-27 13:11:31 +0100

    Fix axios client

--- retry-service/getDelay.js
+++ retry-service/getDelay.js
@@ -2,36 +2,90 @@ const axios = require('axios');
 
 
 class GetDelay {
-
-    TIMEOUT = 10000;
-
+    
 
     constructor(url) {
         this.url = url;
         this.currentRetries = 0;
+        this.TIMEOUT = 2000;
+        this.MAX_RETRIES = 3;
     }
 
  
-    call() {
-        // TODO implement here the logic
-        this.makeRequest();
+   async call() {
+        // Retry while waiting for requests
+        while(this.currentRetries < this.MAX_RETRIES) {
+            console.log(`Call attempt ${this.currentRetries+1} to service`);
+            try {
+                const nextTimeout = this.nextTimeout(); // Timeout must be a fixed amount of time. Only here for testing purposes.
+                await this.makeRequest(nextTimeout);
+                console.log("Regreso de la make request");
+                break;
+            } catch(e) {
+                console.log("Fallo al procesar el make request");
+                this.currentRetries++;
+                const nextSleep = this.nextSleep();
+                console.log("Next sleep: " + nextSleep);
+                await this.sleep(nextSleep);
+            }
+        }
+
+        // Print error message if max retries reached
+        if (this.currentRetries === this.MAX_RETRIES) {
+            console.log(`Max attempt of retries (${this.MAX_RETRIES}) reached`);
+        }
     }
 
 
-    makeRequest(){
-        const httpClient = axios.createClient();
-        httpClient.get(this.url)
-        .then(res => {
+    async makeRequest(timeout){
+        const result = await axios
+        .get(
+            this.url,
+            {timeout}
+        ).then(res => {
             console.log(res.data);
-        })
-        .catch(err => {
-            //console.log(err);
+            return "Done!";
+        }).catch(err => {
             console.log("ERROR:" + err.code);
+            throw new Error("Error while processing request");
         });
+
+        return result;
     }
 
+
+    async sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+
+    nextTimeout() {
+        return this.toMiliseconds(
+            Math.pow(
+                this.toSeconds(this.TIMEOUT),this.currentRetries+1
+            )
+        );
+    }
+
+
+    nextSleep() {
+        return this.toMiliseconds(
+            Math.pow(
+                this.toSeconds(this.TIMEOUT),this.currentRetries
+            )
+        );
+    }
+
+
+    toSeconds(value) {
+        return value / 1000;
+    }
+
+    toMiliseconds(value) {
+        return value * 1000;
+    }
 
 }
 
 
-export default GetDelay;
+module.exports = GetDelay;
--- retry-service/index.js
+++ retry-service/index.js
@@ -1,30 +1,3 @@
-const axios = require('axios');
+const GetDelay = require('./getDelay');
 
-const httpClient = axios.create();
-httpClient.defaults.timeout = 2000;
-
-
-// Execution
-// GET
-httpClient
-    .get('http://localhost:5000/success')
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => {
-        console.error(err);
-    });
-
-    
-
-// POST
-httpClient
-    .get('http://localhost:5000/delay')
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => {
-        //console.log(err);
-        console.log("ERROR:" + err.code);
-
-    })
+new GetDelay('http://localhost:5000/delay').call();
